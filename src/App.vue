9fc4a81547b9ab5dc7a9a07b9cd33f32ff1f07ee
Hubert Skuza <hubert.skuza2011@gmail.com>
2018-08-30 21:18:06 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-6 bg-success">
+          Hey
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style lang="scss" scoped>
+  @import '../node_modules/bootstrap/scss/bootstrap.scss';
+</style>
