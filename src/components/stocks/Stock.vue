59eb25781fd0cccd75f6312ed6a9befc3304cd45
Hubert Skuza <hubert.skuza2011@gmail.com>
2018-09-02 13:41:16 +0200

    Stworzenie komponentu

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,3 +1,39 @@
 <template>
-  <h1>The Stock Component</h1>
+  <div class="col-sm-6 col-md-4">
+    <div class="card my-3">
+      <div class="card-header bg-warning">
+        <h3 class="panel-title">
+          {{stock.name}}: <small>(Price: {{stock.price}})</small>
+        </h3>
+      </div>
+      <div class="card-body d-flex flex-row">
+        <input type="number" class="form-control" placeholder="Quantity" v-model="quantity">
+        <button
+          class="btn btn-success"
+          @click="buyStock"
+          :disabled="quantity <= 0">Buy</button>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+  export default {
+   props: ['stock'],
+   data() {
+     return {
+       quantity: 0
+     }
+   },
+   methods: {
+     buyStock() {
+       const order = {
+         stockId: this.stock.id,
+         stockPrice: this.stock.price,
+         quantity: this.quantity
+       }
+       this.quantity = 0;
+     }
+   },
+  }
+</script>
--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,3 +1,26 @@
 <template>
-  <h1>The Stocks Component</h1>
+  <div class="row">
+    <Stock v-for="stock in stocks" :stock="stock"></Stock>
+  </div>
 </template>
+
+<script>
+
+  import Stock from './Stock.vue';
+
+  export default {
+   data() {
+     return {
+       stocks: [
+         { id: 1, name: 'Facebook', price: 304 },
+         { id: 2, name: 'Twitter', price: 242 },
+         { id: 3, name: 'Google', price: 361 },
+         { id: 4, name: 'Apple', price: 325 }
+       ]
+     }
+   },
+   components: {
+     Stock
+   }
+  }
+</script>
